26d9a1005977ff9673938473f465845c2a770e63
rdurgabhavani045 <rdurgabhavani045@gmail.com>
2025-04-11 01:42:03 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+const scholarships = [
+  {
+    id: 1,
+    name: "Global Excellence Scholarship",
+    country: "USA",
+    field: "Engineering",
+    deadline: "2025-05-30",
+    description: "A full scholarship for outstanding engineering students."
+  },
+  {
+    id: 2,
+    name: "DAAD Research Grant",
+    country: "Germany",
+    field: "Science",
+    deadline: "2025-06-15",
+    description: "Supports international students in scientific research."
+  },
+  {
+    id: 3,
+    name: "Commonwealth Master’s Scholarship",
+    country: "UK",
+    field: "Arts",
+    deadline: "2025-07-25",
+    description: "Fully funded master’s degree in any UK university."
+  },
+  {
+    id: 4,
+    name: "Australia Awards Scholarship",
+    country: "Australia",
+    field: "Pharmacy",
+    deadline: "2025-08-25",
+    description: "Fully funded master’s degree in any Australia university."
+  },
+  {
+    id: 5,
+    name: "Eramus Mundus Joint Masters",
+    country: "Europe",
+    field: "Management And Business Administration",
+    deadline: "2025-09-25",
+    description: "Fully funded master’s degree in any Australia university." 
+  }
+];
